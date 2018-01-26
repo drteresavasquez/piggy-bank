bb8d9b2d8c1fb7ff0553a00486af579d04c3cc7c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-26 17:15:59 -0600

    updated dollars

--- main.js
+++ main.js
@@ -14,5 +14,7 @@ dollarAmount+=piggyBank.pennies*.01;
 dollarAmount+=piggyBank.nickels*.05;
 dollarAmount+=piggyBank.dimes*.10;
 dollarAmount+=piggyBank.quarters*.25;
+piggyBank.dollars = 50;
+dollarAmount+=piggyBank.dollars*1;
 
 console.log("Dollar Amount", `$${dollarAmount}`);
