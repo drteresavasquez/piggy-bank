9805f88e3f7f452993dfa61776b719e8155e0a6d
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-26 14:41:35 -0600

    student help

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("main.js");
+
+let piggyBank = {
+    "pennies": 23,
+    "nickels": 20,
+    "dimes": 10,
+    "quarters": 250
+};
+
+console.log(piggyBank);
+
+let dollarAmount = 0;
+dollarAmount+=piggyBank.pennies*.01;
+dollarAmount+=piggyBank.nickels*.05;
+dollarAmount+=piggyBank.dimes*.10;
+dollarAmount+=piggyBank.quarters*.25;
+
+console.log("Dollar Amount", `$${dollarAmount}`);
